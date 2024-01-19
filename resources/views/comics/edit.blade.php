fda74eb3d02ad8ecc2717e62a656964f7c090712
GianmarcoDandrea <gianm96@hotmail.it>
2024-01-19 16:21:25 +0100

    add: update(wip)

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -6,7 +6,7 @@
 
         <div class="row justify-content-center mt-5">
             <div class="col-6">
-                <form method="POST">
+                <form action="{{ route('comics.update', ['comic' => $comic->id]) }}">
                     @csrf
                     @method('PUT')
                     
