93423c2fb09a95c45148eefc062bea8fc39520a8
GianmarcoDandrea <gianm96@hotmail.it>
2024-01-19 16:04:20 +0100

    add: edit

---
+++ resources/views/comics/edit.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container py-5">
+        <h2>Modify: {{ $comic->title }}</h2>
+
+        <div class="row justify-content-center mt-5">
+            <div class="col-6">
+                <form method="POST">
+                    @csrf
+                    @method('PUT')
+                    
+                    <div class="mb-3">
+                        <label for="title" class="form-label">Title</label>
+                        <input type="text" class="form-control" id="title" name="title" value="{{ $comic->title}}">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="image" class="form-label">Image Url</label>
+                        <input type="text" class="form-control" id="image" name="thumb" value="{{ $comic->thumb}}">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="price" class="form-label">Price</label>
+                        <input type="number" class="form-control" id="price" name="price" value="{{ $comic->price}}">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="series" class="form-label">Series</label>
+                        <input type="text" class="form-control" id="series" name="series" value="{{ $comic->series}}">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="sale_date" class="form-label">Sale Date</label>
+                        <input type="date" class="form-control" id="sale_date" name="sale_date" value="{{ $comic->sale_date}}">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="type" class="form-label">Type</label>
+                        <select id="type" class="form-select" name="type">
+                            <option @selected($comic->type === 'comic_book') value="comic_book">Comic Book</option>
+                            <option @selected($comic->type === 'graphic_novel') value="graphic_novel">Graphic Novel</option>
+                        </select>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="description" class="form-label">Description</label>
+                        <textarea class="form-control" id="description" rows="3" name="description">{{ $comic->description}}</textarea>
+                    </div>
+
+                    <button class="btn btn-success" type="submit">Save</button>
+
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container-fluid w-75 py-5">
+    <div class="container-fluid w-100 p-5">
         <h1>Comics List in Your Database</h1>
         <div class="text-end">
             <a class="btn btn-dark" href="{{ route('comics.create') }}">Add new Comics</a>
@@ -14,7 +14,7 @@
                     <th scope="col">Series</th>
                     <th scope="col">Type</th>
                     <th scope="col">Price</th>
-                    <th scope="col">Description</th>
+                    <th scope="th-lg">Description</th>
                     <th scope="col">Actions</th>
                 </tr>
             </thead>
@@ -26,8 +26,13 @@
                         <td>{{ ucwords($comic->type) }}</td>
                         <td>{{ '$' . $comic->price }}</td>
                         <td>{{ substr($comic->description, 0, 300) . '...' }}</td>
-                        <td>
-                            <a class="btn btn-dark" href="{{ route('comics.show', ['comic' => $comic->id]) }}">Details</a>
+                        <td class="btn-section">
+                            <a class="btn btn-dark" href="{{ route('comics.show', ['comic' => $comic->id]) }}">
+                                <i class="fa-solid fa-info"></i>
+                            </a>
+                            <a class="btn btn-warning" href="{{ route('comics.edit', ['comic' => $comic->id]) }}">
+                                <i class="fa-solid fa-pencil"></i>
+                            </a>
                         </td>
                     </tr>
                 @endforeach
